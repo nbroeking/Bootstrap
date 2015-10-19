2d753079238ea582680ee181002e1a21e7b365be
unknown <matt.bubernak@gmail.com>
2015-10-18 20:58:18 -0600

    modified dir sturcture, added some advanced content

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+function showPage(file) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("presentation_content").innerHTML = xmlhttp.responseText;
+        }
+        else{
+            document.getElementById("presentation_content").innerHTML = "<p>There was an error loading the introduction</p>"
+        }
+    }   
+    xmlhttp.open("GET", "./pages/"+file+".html", true);
+    xmlhttp.send();
+}
+
+
+$( document ).ready(function() {
+    showPage("introduction");
+    return false;
+});
+        
